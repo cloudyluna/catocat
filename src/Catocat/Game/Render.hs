ca1f5265c2357de4eb523a83c8d33c391025d302
cloudyluna <luna.cloudberry@proton.me>
2024-10-04 04:39:51 +0800

    Broken update

--- src/Catocat/Game/Render.hs
+++ src/Catocat/Game/Render.hs
@@ -17,7 +17,6 @@ render gameEnv = liftIO $ do
     RL.clearBackground RL.rayWhite
     RL.drawText (show . _ctrlUp . _controller $ gameEnv) 200 300 50 RL.black
     let texture = fromJust . _texture . _player $ gameEnv
-    let x = vector2'x . _position . _player $ gameEnv
-    RL.drawTextureRec texture (Rectangle 0 0 64 64) (Vector2 x 100) RL.rayWhite
-    RL.drawText (show x) 0 0 50 RL.black
+    RL.drawTextureRec texture (Rectangle 0 0 64 64) (Vector2 100 100) RL.rayWhite
+
     RL.endDrawing
--- src/Catocat/Game/Update.hs
+++ src/Catocat/Game/Update.hs
@@ -10,35 +10,30 @@ import FRP.Yampa
 import Raylib.Core qualified as RL
 import Raylib.Types
 import Raylib.Types qualified as RL
-import Raylib.Util.Math
 
 
 update :: SF GameEnv GameEnv
 update = proc gameEnv -> do
     t <- time -< ()
-
-    let oldPos = _position $ _player gameEnv
-    let player = _player gameEnv
-    let goLeft = player{_position = oldPos |+| Vector2 0 1}
-    let goRight = player{_position = oldPos |-| Vector2 0 1}
-
-    returnA -< gameEnv
+    currentPos <- playerPos -< gameEnv
+    let player = player{_position = currentPos}
+    returnA -< gameEnv{_player = player}
 
 
-controller :: SF () Controller
-controller = notImplemented
+controller :: SF GameEnv Controller
+controller = proc env -> returnA -< _controller env
 
 
-onPress :: (Controller -> Bool) -> a -> SF () (Event a)
+onPress :: (Controller -> Bool) -> a -> SF GameEnv (Event a)
 onPress field a = fmap (fmap (const a)) $ fmap field controller >>> edge
 
 
-playerPos :: SF () Vector2
-playerPos = proc i -> do
-    goUp <- onPress _ctrlUp (Vector2 0 (-1)) -< i
-    goDown <- onPress _ctrlDown (Vector2 0 1) -< i
-    goLeft <- onPress _ctrlLeft (Vector2 (-1) 0) -< i
-    goRight <- onPress _ctrlRight (Vector2 1 0) -< i
+playerPos :: SF GameEnv Vector2
+playerPos = proc env -> do
+    goUp <- onPress _ctrlUp (Vector2 0 (-1)) -< env
+    goDown <- onPress _ctrlDown (Vector2 0 1) -< env
+    goLeft <- onPress _ctrlLeft (Vector2 (-1) 0) -< env
+    goRight <- onPress _ctrlRight (Vector2 1 0) -< env
 
     direction <- hold $ Vector2 0 1 -< asum [goUp, goDown, goLeft, goRight]
     pos <- integral -< direction
@@ -53,7 +48,6 @@ processRaylibKeyboardInputs envState = do
     isKeyADown <- RL.isKeyDown RL.KeyA
     isKeyDDown <- RL.isKeyDown RL.KeyD
 
-    writeIORef envState env
     pure
         env
             { _controller =
