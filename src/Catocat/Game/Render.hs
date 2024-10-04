c57a1c2f3dc2dba9a90ea426b57480821cf4b7ab
cloudyluna <luna.cloudberry@proton.me>
2024-10-05 06:04:42 +0800

    Rename

--- src/Catocat/Game/Render.hs
+++ src/Catocat/Game/Render.hs
@@ -17,11 +17,11 @@ render gameEnv = liftIO $ do
 
     RL.clearBackground RL.rayWhite
 
-    let nTexture = fromJust $ gameEnv ^. player % texture
+    let pTexture = fromJust $ gameEnv ^. player % texture
     let pos = gameEnv ^. player % position
     let x = pos ^. lensVL _x
     let y = pos ^. lensVL _y
-    RL.drawTextureRec nTexture (Rectangle 0 0 64 64) (Vector2 x y) RL.rayWhite
+    RL.drawTextureRec pTexture (Rectangle 0 0 64 64) (Vector2 x y) RL.rayWhite
 
     RL.drawText "YES" 250 250 50 RL.black
 
