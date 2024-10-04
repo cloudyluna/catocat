4a909f1787b8e0fb18a35ccce108ea5bac75e449
cloudyluna <luna.cloudberry@proton.me>
2024-10-05 05:51:44 +0800

    Add new prelude

--- src/Catocat/Prelude.hs
+++ src/Catocat/Prelude.hs
@@ -1,11 +1,30 @@
 module Catocat.Prelude (
     module Control.Monad.IO.Class,
+    module Data.Function,
+    module Optics,
+    module Optics.TH,
+    module Data.IORef,
+    module FRP.Yampa,
+    fromJust,
     pass,
     notImplemented,
 ) where
 
 import Catocat.Prelude.VectorSpace ()
 import Control.Monad.IO.Class
+import Data.Function ((&))
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+import Data.Maybe qualified
+import FRP.Yampa
+import GHC.Stack (HasCallStack)
+import Optics hiding (pre)
+import Optics.TH (makeLenses)
+
+
+-- TODO: Use "in" to put this warning in a warning category in GHC 9.10+.
+{-# WARNING fromJust "Use carefully. Partial function." #-}
+fromJust :: forall a. (HasCallStack) => Maybe a -> a
+fromJust = Data.Maybe.fromJust
 
 
 {-# WARNING notImplemented "'notImplemented' remains in code" #-}
